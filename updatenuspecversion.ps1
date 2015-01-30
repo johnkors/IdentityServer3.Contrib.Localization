f24caf50c79990aa69af202526429a23245e5358
John Korsnes <johnkors@gmail.com>
2015-01-30 21:00:59 +0100

    Fix path to nuspec ps

--- updatenuspecversion.ps1
+++ updatenuspecversion.ps1
@@ -4,7 +4,7 @@ $versionStr = $env:APPVEYOR_BUILD_VERSION
 Write-Host $root
 Write-Host "Setting .nuspec version tag to $versionStr"
 
-$content = (Get-Content $root\source\IdentityServer3.ElasticSearchEventService\IdentityServer3.ElasticSearchEventService.nuspec) 
+$content = (Get-Content $root\source\IdentityServer3.Localization\IdentityServer3.Localization.nuspec) 
 $content = $content -replace '\$version\$',$versionStr
 
-$content | Out-File $root\source\IdentityServer3.ElasticSearchEventService\IdentityServer3.ElasticSearchEventService.nuspec
+$content | Out-File $root\source\IdentityServer3.Localization\IdentityServer3.Localization.nuspec
