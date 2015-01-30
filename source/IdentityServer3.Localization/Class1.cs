899bd624f5577e660bce251fca6d4aaa42ae0a76
John Korsnes <johnkors@gmail.com>
2015-01-30 17:40:12 +0100

    Init

---
+++ source/IdentityServer3.Localization/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IdentityServer3.Localization
+{
+    public class Class1
+    {
+    }
+}
