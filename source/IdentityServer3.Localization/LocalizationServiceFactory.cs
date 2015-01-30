cbe569bbfc09ce1f9dbea08aea89edb4b9a734af
John Korsnes <johnkors@gmail.com>
2015-01-30 22:30:21 +0100

    Removed duplicate constant

--- source/IdentityServer3.Localization/LocalizationServiceFactory.cs
+++ source/IdentityServer3.Localization/LocalizationServiceFactory.cs
@@ -8,18 +8,16 @@ namespace Thinktecture.IdentityServer.Core.Services.Contrib
 {
     public static class LocalizationServiceFactory
     {
-        private const string Default = "Default";
-
         private static readonly IDictionary<string, Func<CultureInfo,ILocalizationService>> AvailableLocalizationServices = new Dictionary<string, Func<CultureInfo,ILocalizationService>>
         {
-            { Default, lang => new DefaultLocalizationService() },
+            { Constants.Default, lang => new DefaultLocalizationService() },
             { "nb-NO", lang => new ResourceFileLocalizationService(lang)}
         };
 
         public static ILocalizationService Create(LocaleOptions options = null)
         {
             var internalOpts = options ?? new LocaleOptions();
-            var choice = string.IsNullOrEmpty(internalOpts.Locale) ? Default : internalOpts.Locale;
+            var choice = string.IsNullOrEmpty(internalOpts.Locale) ? Constants.Default : internalOpts.Locale;
             var culture = new CultureInfo(internalOpts.Locale);
             Func<CultureInfo, ILocalizationService> serviceBuilder;
             var isAvailable = AvailableLocalizationServices.TryGetValue(choice, out serviceBuilder);
