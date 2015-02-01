d962caf77beb75b0d059720839a3f5580b2fd12f
John Korsnes <johnkors@gmail.com>
2015-02-01 18:18:17 +0100

    translation.html

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,9 @@
 console.log('This would be the main JS file.');
+
+var nameApp = angular.module('TestApp', []);
+
+nameApp.controller('LocaleCtrl', function ($scope, $http){
+  $http.get('http://localeapi.apphb.com').success(function(data) {
+    $scope.translations = data;
+  });
+});
