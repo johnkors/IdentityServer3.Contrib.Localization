6425320ec178f5966bcc01023396206bcaa53288
John Korsnes <johnkors@gmail.com>
2015-02-01 19:22:31 +0100

    test view

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,9 +1,39 @@
 console.log('This would be the main JS file.');
 
-var nameApp = angular.module('TestApp', []);
+var nameApp = angular.module('TestApp', ['ngRoute']);
 
-nameApp.controller('LocaleCtrl', function ($scope, $http){
+nameApp.config(function($routeProvider) {
+  $routeProvider.
+    when('/', {
+      templateUrl: 'locales.html',
+      controller: 'LocalesCtrl'
+    }).
+    when('/:locale', {
+      templateUrl: 'localeDetails.html',
+      controller: 'LocaleDetailsCtr'
+    }).
+    otherwise({
+      redirectTo: '/'
+    });
+});
+
+nameApp.controller('LocalesCtrl', function ($scope, $http){
   $http.get('http://localeapi.apphb.com').success(function(data) {
     $scope.translations = data;
   });
+});
+
+nameApp.controller('LocaleDetailsCtr', function ($scope, $http, $routeParams){
+  $scope.locale = $routeParams.locale;
+  $http.get('http://localeapi.apphb.com/').success(function(data) {
+      $scope.translations = data;
+      var details = data.filter(function(entry){
+        return entry.locale === $scope.locale;
+      })[0];
+
+      $scope.localeDetails = details;
+  });
+
+
+
 });
