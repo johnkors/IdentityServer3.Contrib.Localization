3c7b018bf4e8cb1afd229113e12d1cf61ced0292
John10 Korsnes <johnkors@gmail.com>
2016-08-21 18:32:35 +0200

    Explicit en-GB, en-US

--- source/Unittests/AvailableTranslations.cs
+++ source/Unittests/AvailableTranslations.cs
@@ -9,24 +9,27 @@ namespace Unittests
         [Theory]
         [InlineData("Default")]
         [InlineData("pirate")]
+        [InlineData("ar-SA")]
+        [InlineData("cs-CZ")]
         [InlineData("de-DE")]
+        [InlineData("da-DK")]
+        [InlineData("en-GB")]
+        [InlineData("en-US")]
         [InlineData("es-AR")]
+        [InlineData("es-ES")]
+        [InlineData("fi-FI")]
         [InlineData("fr-FR")]
+        [InlineData("it-IT")]
         [InlineData("nb-NO")]
+        [InlineData("nl-NL")]
+        [InlineData("pl-PL")]
+        [InlineData("pt-BR")]
+        [InlineData("ro-RO")]
+        [InlineData("ru-RU")]
+        [InlineData("sk-SK")]
         [InlineData("sv-SE")]
         [InlineData("tr-TR")]
-        [InlineData("ro-RO")]
-        [InlineData("nl-NL")]
         [InlineData("zh-CN")]
-        [InlineData("da-DK")]
-        [InlineData("ru-RU")]
-        [InlineData("pt-BR")]
-        [InlineData("cs-CZ")]
-        [InlineData("it-IT")]
-        [InlineData("pl-PL")]
-        [InlineData("sk-SK")]
-        [InlineData("ar-SA")]
-        [InlineData("fi-FI")]
         public void ContainsLocales(string locale)
         {
             Assert.Contains(GlobalizedLocalizationService.GetAvailableLocales(), s => s.Equals(locale));
@@ -35,7 +38,7 @@ namespace Unittests
         [Fact]
         public void HasCorrectCount()
         {
-            Assert.Equal(20, GlobalizedLocalizationService.GetAvailableLocales().Count());
+            Assert.Equal(23, GlobalizedLocalizationService.GetAvailableLocales().Count());
         }
     }
 }
