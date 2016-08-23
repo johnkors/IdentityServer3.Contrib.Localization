1146fd4a0dd9a8b09ebc3a418d3ee264aaabc8da
john4ice <john.korsnes@ice.net>
2016-08-23 09:29:03 +0200

    Updated with latest API changes

--- javascripts/main.js
+++ javascripts/main.js
@@ -22,30 +22,43 @@ nameApp.config(function($routeProvider) {
     });
 });
 
-nameApp.controller('LocalesCtrl', function ($scope, $http){
-  $http.get('http://localeapi.apphb.com').success(function(data) {
-    $scope.translations = data;
+nameApp.controller('LocalesCtrl', function ($scope, $http, $location, $window){
+  
+  $http.get('http://localhost:7799/language').then(    
+    function(res) { 
+      $scope.locale = res.data;
+      $location.url($scope.locale);
+    }, 
+    function(data){ 
+      console.log("fuck! Falling back to swearing in danish.");
+      $scope.locale = "de-DK";
   });
+  
+  $scope.submitted = function(){
+      console.log("submitted!");
+      $location.url($scope.locale);
+  }
 });
 
 nameApp.controller('LocaleDetailsCtr', function ($scope, $http, $routeParams, $location, $anchorScroll){
 
-  $scope.locale = $routeParams.locale;
-  $http.get('http://localeapi.apphb.com/').success(function(data) {
-      $scope.translations = data;
-      var details = data.filter(function(entry){
-        return entry.locale === $scope.locale;
-      })[0];
+    $scope.given = $scope.locale;
 
-      $scope.localeDetails = details;
-  });
-   var old = $location.hash();
+    $http.get('http://localhost:7799/locale/' + $scope.locale).then(    
+      function(res) { 
+          $scope.fetched = res.data.locale;       
+          console.log(res);
+          $scope.translations = res.data.translations;
+          $scope.headerlanguage = res.data.headerLanguage        
+          $scope.localeDetails = res.data;
+      }, 
+      function(data){ 
+        console.log("fuck!")
+    });
+    
+    var old = $location.hash();
     $location.hash('details');
     $anchorScroll();
     //reset to old to keep any additional routing logic from kicking in
     $location.hash(old);
-
-
-
-
 });
